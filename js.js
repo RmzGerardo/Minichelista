f3862d42374e8cc1bd9f949e7e8ba9fbd931886b
RmzGerardo <lalitomendez132mail.com>
2024-10-21 02:45:50 -0600

    Primer version de la pagina 21 / 10 / 2024

--- js.js
+++ js.js
@@ -1,5 +1,4 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.materialboxed');
-//     var instances = M.Materialbox.init(elems, options);
-    
-//   });
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+  });
